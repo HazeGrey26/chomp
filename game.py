ee5695e34fe3c45ced3ced6a5b2687bf10b7aa00
HazeGrey26 <147719792+HazeGrey26@users.noreply.github.com>
2023-10-12 09:44:28 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,17 @@
-import math
 import pygame
+import time
 
-math.sin(4)
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption(('Chomp!'), ('Chomp!'))
+
+screen.fill("blue")
+pygame.draw.rect(screen, ("brown"), (0, 380, 400, 400))
+pygame.draw.rect(screen, ("green"), (200, 220, 30, 10))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == 256:
+            print("NO")
