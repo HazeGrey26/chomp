7837be98c939e484a5b914c573df4db5d88f9199
HazeGrey26 <147719792+HazeGrey26@users.noreply.github.com>
2023-10-18 09:27:34 -0400

    Used parametric variables and added fish!

--- game.py
+++ game.py
@@ -1,17 +1,28 @@
+import sys
 import pygame
-import time
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = SCREEN_HEIGHT-20
+PLAYER_POSITION = (SCREEN_WIDTH/2-32, SCREEN_HEIGHT/2-32)
+
+# Colors
+WATER_COLOR = "blue"
+SAND_COLOR = "brown"
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
-pygame.display.set_caption(('Chomp!'), ('Chomp!'))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Chomp!', 'Chomp!')
 
-screen.fill("blue")
-pygame.draw.rect(screen, ("brown"), (0, 380, 400, 400))
-pygame.draw.rect(screen, ("green"), (200, 220, 30, 10))
+screen.fill(WATER_COLOR)
+sand = pygame.image.load("assets/images/fishTile_080.png")  #.convert()  # Convert speeds up image loading, but removes the transparency from pngs
+screen.blit(sand, PLAYER_POSITION)
+pygame.draw.rect(screen, SAND_COLOR, (0, SAND_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.flip()
 
 while True:
     for event in pygame.event.get():
-        if event.type == 256:
-            print("NO")
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
